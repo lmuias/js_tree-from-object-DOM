1f026eb5447d386a0422a966dacd8bf587b9e249
Valentyn Sobol <valiksobol14@gmail.com>
2024-06-19 12:43:53 +0300

    make build

---
+++ index.07b7557b.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in r)if(r.hasOwnProperty(a)){var l=document.createElement("li");l.textContent=a;var i=r[a];Object.keys(i).length>0&&e(l,i),t.appendChild(l)}n.appendChild(t)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.07b7557b.js.map
